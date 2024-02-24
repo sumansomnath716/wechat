00013968480a5a7172182dde9e3fd3406f322d54
sumansomnath716 <sumansomnath716@gmail.com>
2024-02-24 21:53:56 +0530

    Somnath commit with signin and signup

--- src/screens/auth/Auth.js
+++ src/screens/auth/Auth.js
@@ -5,7 +5,7 @@ function Auth() {
   return (
     <>
       <div className="bg-green-800 flex justify-center h-screen w-screen items-center">
-        <div className="bg-white w-5/12 h-auto rounded-lg text-black p-8 font-semibold space-y-3 text-xl shadow-md">
+        <div className="bg-white w-96 h-auto rounded-lg text-black p-8 font-semibold space-y-3 text-xl shadow-md">
           <p className="text-gray-500 font-bold">Sign in to our platform</p>
           <Outlet />
         </div>
