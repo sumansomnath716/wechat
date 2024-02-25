3ba83942fee14551db3179583f25afcc24f12d2a
sumansomnath716 <sumansomnath716@gmail.com>
2024-02-25 19:59:13 +0530

    somnath commit

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,11 +1,33 @@
-import React from 'react'
-
+import React from "react";
+// import chatImg from '';
 const Main = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="min-h-screen min-w-screen relative">
+        {/* Background Portion */}
+        <div
+        className="bg-gradient-to-r
+        from-green-700 absolute top-0
+        to-green-900
+          w-full h-52"></div>
+        {/* End */}
 
-export default Main
+        {/* Chat Window */}
+        <div className="h-[calc(100%_-_0.75rem)] p-5 w-full absolute top-3 bg-transparent">
+              <div className="bg-white h-full w-full shadow-xl border-slate-500">
+                      <div className="grid grid-cols-3 gap-0 h-full w-full">
+                          <div className="col-span-1 w-full min-h-full overflow-y-auto">01</div>
+                          <div className="col-span-2 
+                          bg-[url('../../../src/Assets/Images/chat.png')]
+                          overflow-y-auto"></div>
+
+                      </div>
+              </div>
+        </div>
+        {/* End */}
+      </div>
+    </>
+  );
+};
+
+export default Main;
