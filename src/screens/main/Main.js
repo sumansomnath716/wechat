cbda868444e384521c48b44e2dc10cdc33f0e501
sumanmitra121 <sumanmitra0096@gmail.com>
2024-03-09 13:53:28 +0530

    dev1_09_03_2024

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -62,10 +62,10 @@ const Main = () => {
                             overflow-hidden bg-neutral-200 rounded-full dark:bg-gray-600
                             w-8 sm:w-8 h-8 sm:h-8 rounded-full
                             py-3">
-                          <span className="font-bold text-gray-600 dark:text-gray-300 text-xs">SSM</span>
+                          <span className="font-medium text-green-900 dark:text-green-800 text-xs">SS</span>
                         </div>
                         <div className="flex flex-col">
-                          <span className="text-xs font-medium">Suman Somnath Mitra</span>
+                          <p className="text-xs font-medium overflow-hidden truncate w-28">Somnath Suman</p>
                           <span className="text-[10px] font-medium">01:30 PM</span>
 
                         </div>
@@ -547,81 +547,12 @@ const Main = () => {
                           py-2 pr-10 pl-3"
                       />
                       <div className="absolute right-0  items-center inset-y-0 hidden sm:flex">
-                        {/* <button
-                            type="button"
-                            className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-white focus:outline-none"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                              className="h-6 w-6 text-green-900 font-bold "
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                              ></path>
-                            </svg>
-                          </button>
-                          <button
-                            type="button"
-                            className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-white focus:outline-none"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                              className="h-6 w-6 text-green-900 font-bold "
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
-                              ></path>
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
-                              ></path>
-                            </svg>
-                          </button> */}
-                        {/* <button
-                            type="button"
-                            className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-white focus:outline-none"
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                              className="h-6 w-6 text-green-900 font-bold "
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                              ></path>
-                            </svg>
-                          </button> */}
+                       
                         <button
                           type="button"
                           className="inline-flex items-center justify-center rounded-md h-12 w-12 transition duration-500 ease-in-out text-green-900 font-bold focus:outline-none"
                         >
-                          {/* <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                              className="h-6 w-6 ml-2 transform rotate-90"
-                            >
-                              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                            </svg> */}
+                        
                           <span className="material-symbols-outlined text-3xl">
                             send
                           </span>
